27b57056d58c40fd376d8a6cbad242fe0bedaad7
Vinit Patel <vinitpatel24072003@gmail.com>
2025-04-21 13:17:31 +0530

    Update server.js

--- food-del/backend/server.js
+++ food-del/backend/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: 'https://swigato-1crt.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'token', 'x-access-token']
